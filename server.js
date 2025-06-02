fc4afb2a424c1eefdbd805953f9144a388cea30a
BekDevIOS <umekhmonboev@gmail.com>
2025-06-02 18:03:34 +0900

    feat: create devolop branch

--- server.js
+++ server.js
@@ -1,6 +1,16 @@
 const express = require('express');
 const http = require('http');
 const app = express();
+const fs = require('fs');
+
+let user;
+fs.readFile("database/user.json", "utf-8", (err, data) => {
+    if(err){
+        console.log('ERROR:', err);
+    } else {
+        user = JSON.parse(data);
+    }
+});
 
 // 1: Kirish code
 app.use(express.static('public'));
@@ -15,13 +25,13 @@ app.set('views', 'views');
 app.set('view engine', 'ejs');
 
 // 4: Routing code
-app.post('/create-item', (req, res) => {
-    console.log(req.body);
-    res.json({name: "Jay"});
-})
+// app.post('/create-item', (req, res) => {
+//     console.log(req.body);
+//     res.json({name: "Jay"});
+// });
 
 app.get('/', function (req, res) {
-    res.render('harid');
+    res.render('harid', {user: user});
 });
 
 const server = http.createServer(app);
